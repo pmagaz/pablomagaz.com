f417172f6fb4841ad9462c514b830a02d757ecdf
Fernando Ramos <fernando.ramos@onelog.ch>
2024-04-19 10:16:34 +0200

    chore: render blog list with links to blog posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import type { PageProps } from "gatsby";
-
-type DataProps = {};
 
 const Blog = ({ data }: PageProps<any>) => {
   console.log({ data });
 
-  return data?.allMdx.nodes.map((nodeFile: any) => (
-    <article key={nodeFile?.id}>
-      Title: <span>{nodeFile.frontmatter.title}</span>
-      <br />
-      Tags: <strong>{nodeFile.frontmatter.tags}</strong>
-      <br />
-      <br />
-    </article>
-  ));
+  return (
+    <>
+      <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+        Blog posts:
+      </h2>
+      <ul className="max-w-md space-y-1 text-gray-900 list-disc list-inside dark:text-gray-400">
+        {data?.allMdx.nodes.map((nodeFile: any) => (
+          <li className="text-gray-500 dark:text-gray-400" key={nodeFile?.id}>
+            <Link
+              to={`/blog/${nodeFile.frontmatter.slug}`}
+              className="inline-flex items-center font-medium text-blue-700 dark:text-blue-500 hover:underline"
+            >
+              {nodeFile.frontmatter.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export const query = graphql`
