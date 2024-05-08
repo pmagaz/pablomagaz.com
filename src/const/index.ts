b48e52e315ee7b922279edbcfc905db5d78ccf84
Fernando Ramos <fernando.ramos@onelog.ch>
2024-05-08 09:19:59 +0200

    chore: add metadata

---
+++ src/const/index.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "El Blog Isomórfico";
+export const SITE_AUTHOR = "Pablo Magaz";
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql, HeadProps, Link } from "gatsby";
 import { MDXProvider } from "@mdx-js/react";
 import type { PageProps } from "gatsby";
 import { Code as code } from "../components/code";
 import Layout from "../components/layout";
 import "../styles/global.css";
 import "./post.css";
+import { SITE_AUTHOR, SITE_TITLE } from "../const";
 
-const PageTemplate = ({ data, children }: PageProps<any>) => (
+type DataProps = {
+  mdx: {
+    frontmatter: {
+      title: string;
+      date_published: string;
+      tags: string;
+      slug: string;
+    };
+  };
+};
+
+const PageTemplate = ({ data, children }: PageProps<DataProps>) => (
   <Layout>
     <h1>{data.mdx.frontmatter.title}</h1>
     <div className="frontmatter-data-container">
-      <span className="frontmatter-author">Pablo Magaz</span>
+      <span className="frontmatter-SITE_author">Pablo Magaz</span>
       <time className="frontmatter-time">
         {data.mdx.frontmatter.date_published}
       </time>
@@ -27,11 +39,29 @@ const PageTemplate = ({ data, children }: PageProps<any>) => (
 
 export default PageTemplate;
 
+export const Head = ({ data }: HeadProps<DataProps>) => (
+  <>
+    <title>
+      {SITE_AUTHOR} - {data.mdx.frontmatter.title}
+    </title>
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content={data.mdx.frontmatter.title} />
+    <meta property="og:site_name" content={SITE_TITLE} />
+    <meta
+      property="og:url"
+      content={`https://pablomagaz.com/blog/${data.mdx.frontmatter.slug}`}
+    />
+  </>
+);
+
 export const query = graphql`
   query ($id: String!) {
     mdx(id: { eq: $id }) {
       frontmatter {
         tags
+        slug
         title
         date_published(formatString: "d-MMM-YYYY", locale: "es")
       }
