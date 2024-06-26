9d1252043610cda901deadff3c52758675de05d5
Pablo <magazpablo@gmail.com>
2024-06-26 14:14:04 +0200

    chore: new version

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My landing page`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Pablo Magaz's Personal Website`,
+    siteUrl: `https://www.pablomagaz.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
